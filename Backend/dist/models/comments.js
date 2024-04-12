aee0243ca878faf5ca3355d236999f9a01f73356
Maurice <balindamoris@gmail.com>
2024-04-12 19:54:51 +0200

    changes

---
+++ Backend/dist/models/comments.js
@@ -0,0 +1,10 @@
+import mongoose, { Schema } from 'mongoose';
+// Define the schema for the Comment model
+const commentSchema = new Schema({
+    // Assuming postId references a Post model
+    text: { type: String, required: true },
+});
+// Create the Comment model
+const CommentModel = mongoose.model('Comment', commentSchema);
+export default CommentModel;
+//# sourceMappingURL=comments.js.map
